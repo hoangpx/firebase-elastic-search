396c14e0970e813861c6f90896564a46c37ac567
Hoang Pham <hoangpx@fsoft.com.vn>
2016-06-15 11:38:22 -0600

    test2

--- config.js
+++ config.js
@@ -10,7 +10,7 @@
  ***************************************************/
 //process.env.FB_NAME = 'neighborstool';
 // Your Firebase instance where we will listen and write search results
-exports.FB_URL   = 'https://' + process.env.FB_NAME + '.firebaseio.com/';
+exports.FB_URL   = 'https://' + process.env.FB_NAME + 'asdasd.firebaseio.com/';
 
 // Either your Firebase secret or a token you create with no expiry, used to authenticate
 // To Firebase and access search data.
